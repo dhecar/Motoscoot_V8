ac3cc8f17936219784a06643aafb275ac0717b68
dhecar <dhecar@gmail.com>
2016-08-24 17:52:17 +0200

    [IMP] from openerp.osv import fields, osv

--- sine_partner_history/history.py
+++ sine_partner_history/history.py
@@ -20,7 +20,7 @@
 #
 ##############################################################################
 
-from osv import fields, osv
+from openerp.osv import fields, osv
 
 
 class res_partner(osv.osv):
