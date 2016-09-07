fb064492297ab7f056438e0660f9c55a5da2e3e7
dhecar <dhecar@gmail.com>
2016-09-07 17:00:40 +0200

    NEW UPDATE

--- sine_motoscoot_sales/models/sale.py
+++ sine_motoscoot_sales/models/sale.py
@@ -23,7 +23,7 @@ from openerp import models, fields, api, exceptions
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # sum_stock = fields.Char(related='product_id.stock_by_loc', string='Stocks')
+    sum_stock = fields.Char(related='product_id.stock_by_loc', string='Stocks')
     incoming = fields.Float(related='product_id.incoming_qty', string='IN')
     outgoing = fields.Float(related='product_id.outgoing_qty', string='OUT')
     product_id = fields.Many2one(comodel_name='product.product', string='Product', domain=[('sale_ok', '=', True)],
