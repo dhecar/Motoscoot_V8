9e58803d39eed76db123099dad496538158dc4c0
dhecar <dhecar@gmail.com>
2016-09-07 16:59:14 +0200

    NEW UPDATE

--- sine_motoscoot_sales/__openerp__.py
+++ sine_motoscoot_sales/__openerp__.py
@@ -6,7 +6,7 @@
     "category" : "Generic Modules/Others",
     "website" : "http://sinergiainformatica.net",
     "description": "Extension módulo sale",
-    "depends" : ["sale"],
+    "depends" : ["sale","base_delivery_carrier_files"],
     # "data" : ["views/sale_view.xml","views/partner_motoscoot_view.xml"],
     "data" : ["views/sale_view.xml"],
     "active": True,
