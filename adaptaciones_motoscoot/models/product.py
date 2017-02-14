2b06defb8c567f9ab664c032001adc8253466f42
dhecar <dhecar@gmail.com>
2017-02-14 13:48:37 +0100

    [IMP] migrated

--- adaptaciones_motoscoot/models/product.py
+++ adaptaciones_motoscoot/models/product.py
@@ -90,7 +90,7 @@ class ProductTemplate(models.Model):
     label_print = fields.Boolean(string='Label Print?', help='Se debe imprimir la etiqueta en albaranes de entrada?',
                                  default='True')
     qty_total = fields.Integer(compute=_compute_total_qty, string='Stock Total')
-    locs_ids = fields.Many2many(comodel_name='extrainfo.locations',inverse_name='extra_loc', ondelete='restrict')
+    locs_ids = fields.Many2many(comodel_name='extrainfo.locations', ondelete='restrict')
 
 
 
